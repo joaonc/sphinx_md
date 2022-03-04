c9838baee367540670c9e1c3334924284ca246d9
Joao Coelho <joaonc@users.noreply.github.com>
2022-03-04 14:18:28 -0600

    feat: Type hints in docs (#5)

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,8 @@ author = 'Joao Coelho'
 # ones.
 extensions = [
     'myst_parser',
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -38,7 +39,7 @@ templates_path = ['docs/_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
 
 
 # -- Options for HTML output -------------------------------------------------
